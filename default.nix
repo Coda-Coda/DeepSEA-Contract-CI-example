3fdb2d7ac261a83df3e094b1d07dd6ea9c202c92
Daniel Britten <daniel.britten@outlook.com>
2021-05-21 21:00:13 +1200

    Update DeepSEA

--- default.nix
+++ default.nix
@@ -7,8 +7,8 @@ let pkgs = import (builtins.fetchTarball {
 let deepSEA-src = ( pkgs.fetchFromGitHub {
     owner  = "Coda-Coda";
     repo   = "deepsea-1";
-    rev    = "a0ee741c9d396aa79ae04f6d3cda0811f3236ca3";
-    sha256 = "19xrfv5nmrxm42psd0jwn0qadal9ayn528fcqa1fixhbihml44ch"; } );
+    rev    = "b37adedb166275effe2b0b16a0a42f9b65234375";
+    sha256 = "1dn6ad3gq7k0vszidw7cg0yxja7l5c8ynv5fsa24z4i1x1f6k7ia"; } );
     # To update the sha256 run:
     # nix-prefetch-url --unpack https://github.com/Coda-Coda/deepsea-1/archive/commitHashGoesHere.tar.gz
     # Then replace sha256 = ... with the last string of characters returned by nix-prefetch-url
